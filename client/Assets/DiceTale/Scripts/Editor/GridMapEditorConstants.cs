e328bd5a7d690ceb9709e465a3540fdeef0582da
kadu <qq906832102@gmail.com>
2026-08-26 10:27:26 +0800

    feat(editor): add GridMapEditorConstants for shared paths and display constants

---
+++ client/Assets/DiceTale/Scripts/Editor/GridMapEditorConstants.cs
@@ -0,0 +1,12 @@
+namespace DiceTale.Editor
+{
+    public static class GridMapEditorConstants
+    {
+        public const float CellDisplaySize = 24f;
+        public const string ImageDirectory = "Assets/DiceTale/Res/Textures";
+        public const string DataDirectory = "Assets/DiceTale/Resources";
+        public const string DataDirectoryFull = "DiceTale/Resources";
+        public const int MinBrushSize = 1;
+        public const int MaxBrushSize = 5;
+    }
+}
